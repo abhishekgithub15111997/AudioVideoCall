2fd617b71724ced9f7eb50339fe63f63f08db059
Abhikumar1511 <65035617+Abhikumar1511@users.noreply.github.com>
2020-10-10 12:50:19 +0530

    Updates

---
+++ precache-manifest.7a9c35e7c69c6149b973bf4d5862d6ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf34b498f7413992b0af2151aa6449cf",
+    "url": "/AudioVideoCall/index.html"
+  },
+  {
+    "revision": "407de6ed9bc2e94b6f0f",
+    "url": "/AudioVideoCall/static/css/main.f93f894b.chunk.css"
+  },
+  {
+    "revision": "cb19b7317ca6ece8d659",
+    "url": "/AudioVideoCall/static/js/2.7c6a2eea.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/AudioVideoCall/static/js/2.7c6a2eea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "407de6ed9bc2e94b6f0f",
+    "url": "/AudioVideoCall/static/js/main.fd0b4e7b.chunk.js"
+  },
+  {
+    "revision": "e1ef86aefd7a719f36e6",
+    "url": "/AudioVideoCall/static/js/runtime-main.48652d05.js"
+  }
+]);
